b8179937bef6881d273c26a539cfd478e6025ee3
gurii-dot <wlflvus@gmail.com>
2026-09-15 11:06:45 +0900

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "net.pokerogue.app",
-  appName: "PokéRogue",
+  appName: "PokéRogue TEST",
   webDir: "dist",
   server: {
     androidScheme: "https",
